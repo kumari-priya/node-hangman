9f386ab1c95e4613d68228c6bc2838afca85cea0
Kumari Priya <priya@goel.pw>
2017-11-08 19:57:21 -0800

    app js

---
+++ app.js
@@ -0,0 +1,19 @@
+import Word from 'word';
+
+class Game extends Word{
+  constructor(word,letter,attempts,reset) {
+    this.words = ['SQL','Javasript','JQuery','Bootstrap','Ruby on Rails','Python Django','Angular','Node','React'];
+    this.index = 0;
+    this.letter = letter;
+    this.attempts = attempts;
+    this.reset = reset;
+  }
+  checkLetter(){
+
+
+  }
+  nextWord(){
+
+
+  }
+}
