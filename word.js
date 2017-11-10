085879ef443ee0d783e989de06f63dcbd9e1937a
Kumari Priya <priya@goel.pw>
2017-11-09 23:02:11 -0800

    word module

--- word.js
+++ word.js
@@ -1,40 +1,62 @@
+// require the Letter module
 const Letter = require('./letter');
 
-let Word = (word) => {
+function Word(word) {
   this.word = word;
+  this.displayWord = '';
   this.allLetters = [];
-  this.guessesLetters = [];
   this.attempts = 5;
 
-
-  let blankSpace = () => {
-    allLetters = word.split('');
-    guessesLetters = wordArr.map(function(i) {
-      let objLetter = new Letter(i);
-      allLetters.push(objLetter)
-      return objLetter.blankSpace();
+// generate blank space
+  this.blankSpace = () => {
+    let wordArr = word.split('');
+    // store 'this' in a variable to avoid conflicts
+    let thisWord = this;
+    wordArr.forEach(function(element, index) {
+      let objLetter = new Letter(element);
+      thisWord.allLetters.push(objLetter)
+      thisWord.displayWord += objLetter.getResult();
     });
-    this.attempts += allLetters.length;
-    return guessesLetters;
-    // guessedWord = blankWord.join('');
-    // return guessedWord;
+    this.attempts += this.allLetters.length;
+    return this.displayWord;
   };
 
-  let check = (letter) => {
-    this.attempts--;
-    if (guessesLetter.indexOf(letter) !== -1) {
-      if (allLetters.indexOf(letter) > -1) {
-        allLetters.forEach(function(element, index) {
-          if (element == letter) {
-            guessesLetter[index] = element;
+}
+
+// prototype function to check the status after every letter entered
+Word.prototype.check = function(letter) {
+      let flag = 0;
+      // store 'this' in a variable to avoid conflicts
+      let thisWord = this;
+      if (this.displayWord.toLowerCase().indexOf(letter) == -1) {
+        if (this.word.toLowerCase().indexOf(letter) > -1) {
+          this.allLetters.forEach(function(element, index) {
+            if (element.letter.toLowerCase() == letter.toLowerCase()) {
+              element.replace();
+              thisWord.displayWord = thisWord.displayWord.substr(0, index) + element.getResult() + thisWord.displayWord.substr(index + 1);
+              flag = 1;
+            }
+          });
+          // user gussed the letter right
+          if(flag ==1){
+            console.log('Correct!')
           }
-        });
-        // if(guessesLetter.join('') === allLetters.join('')){
-        //
-        // }
-      }
-    }
-  };
-};
+        }
+        // user gussed the letter wrong
+        else{
+          console.log('Incorrect!')
+          this.attempts--;
+          console.log(this.attempts+ ' Gusses left!');
 
+        }
+      }
+      // user had already gussed the letter
+      else{
+          this.attempts--;
+          console.log('Already Gussed!')
+          console.log(this.attempts+ ' Gusses left!');
+      }
+        return thisWord.displayWord;
+};
+// export the module
 module.exports = Word;
